ccb82828aaa188dbfc3b998b18b80b39e649af82
dominhkha <kha.minhdo199@gmail.com>
2022-02-23 12:02:07 +0700

    remove redundants

--- mainTest.cpp
+++ mainTest.cpp
@@ -6,7 +6,6 @@
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 
-
 #include "hello.h"
 
 class Test : public CPPUNIT_NS::TestCase
@@ -33,26 +32,6 @@ protected:
    }
   }
 };
-
-
-CPPUNIT_TEST_SUITE_REGISTRATION(Test);
-
-int main()
-{
-  CPPUNIT_NS::TestResult controller;
-
-  CPPUNIT_NS::TestResultCollector result;
-  controller.addListener(&result);
-
-  CPPUNIT_NS::BriefTestProgressListener progress;
-  controller.addListener(&progress);
-
-  CPPUNIT_NS::TestRunner runner;
-  runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
-  runner.run(controller);
-
-  return result.wasSuccessful() ? 0 : 1;
-}
 
 
 CPPUNIT_TEST_SUITE_REGISTRATION(Test);
